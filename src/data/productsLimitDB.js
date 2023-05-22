58089a6da8a708dca5180617a49e6d65cdb62e69
YanovskiyNV <mister.frontender@gmail.com>
2023-05-23 02:19:04 +0300

    Первоначальная версия

---
+++ src/data/productsLimitDB.js
@@ -0,0 +1,36 @@
+const productsLimit = [
+	{
+		productId: 1,
+		maxAmount: 1,
+	},
+	{
+		productId: 2,
+		maxAmount: 1,
+	},
+	{
+		productId: 3,
+		maxAmount: 1,
+	},
+	{
+		productId: 4,
+		maxAmount: 1,
+	},
+	{
+		productId: 5,
+		maxAmount: 1,
+	},
+	{
+		productId: 6,
+		maxAmount: 1,
+	},
+	{
+		productId: 7,
+		maxAmount: 1,
+	},
+	{
+		productId: 8,
+		maxAmount: 1,
+	},
+];
+
+export default productsLimit;
